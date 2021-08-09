94236651789c6c1a533a43f2c8844760f5c50866
cheikhoumou <67443473+cheikhoumou@users.noreply.github.com>
2021-08-09 20:30:10 +0200

    freelance five star
    freelance five star

---
+++ Neme.js
@@ -0,0 +1,36 @@
+window.li1.innerHTML = '<li>محترف برمجة</li>';
+window.li1.href = 'https://khamsat.com/user/cheikoumou';
+// index 1
+window.li2.innerHTML = '<li>مبرمج تطبيقات</li>';
+window.li2.href = 'https://khamsat.com/user/cheikoumou';
+// index 2
+window.li3.innerHTML = '<li>مترجم</li>';
+window.li3.href = '#';
+// index 3
+window.li4.innerHTML = '<li> مسوق</li>';
+window.li4.href = 'https://khamsat.com/user/sidati96';
+// index 4
+window.li5.innerHTML = '<li>مصمم جرفك</li>';
+window.li5.href = '#';
+// index 5
+window.li6.innerHTML = '<li>مبرمج العاب</li>';
+window.li6.href = '#';
+// index 6
+window.li7.innerHTML = '<li>مهندس</li>';
+window.li7.href = '#';
+// index 7
+window.li8.innerHTML = '<li>مصمم فيديوهات</li>';
+window.li8.href = '#';
+// index 8
+window.li9.innerHTML = '<li>كاتب</li>';
+window.li9.href = 'https://khamsat.com/user/awah';
+// index 9
+window.li10.innerHTML = '<li>هندسة صوتية</li>';
+window.li10.href = '#';
+// index 10
+window.li11.innerHTML = '<li>محترف بلوجر</li>';
+window.li11.href = 'https://khamsat.com/user/cheikoumou';
+// index 11
+window.li12.innerHTML = '<li>مصمم مواقع</li>';
+window.li12.href = 'https://khamsat.com/user/cheikoumou';
+// index 12
